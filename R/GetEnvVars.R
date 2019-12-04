8f35da9521e0002b037cd86695a5c2b2b65209ec
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2019-12-04 18:57:16 +0300

    use GetEnvVars.R for getting PATH and correspond library path
    R-435 R-384
    
    GitOrigin-RevId: 246291f250ef667230778cf242258976edc54333

--- R/GetEnvVars.R
+++ R/GetEnvVars.R
@@ -14,5 +14,26 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https:#www.gnu.org/licenses/>.
 
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
 
-cat(R.home(), R.home('share'), R.home('include'), R.home('doc'), sep=.Platform$path.sep)
+getLDLibraryPath <- function() {
+  if (get_os() == "osx") Sys.getenv("DYLD_FALLBACK_LIBRARY_PATH")
+  else if (get_os() == "linux") Sys.getenv("LD_LIBRARY_PATH")
+  else ""
+}
+
+cat(R.home(), R.home('share'), R.home('include'), R.home('doc'), Sys.getenv("PATH"), getLDLibraryPath(), sep='\n')
