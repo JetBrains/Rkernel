0ea8bfb0edccc6460d58f0bdd31d7861379b2216
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2019-12-18 19:39:11 +0300

    add initial conda support
    GitOrigin-RevId: a4b4eb85cee1a7013979d38fc38b8592d42b95c6

--- build_rwrapper.sh
+++ build_rwrapper.sh
@@ -19,6 +19,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
   PACKAGE_NAME="x64-linux"
 else
   PACKAGE_NAME="x64-osx"
+  IS_MACOS=true
 fi
 
 # patch Rcpp
@@ -37,4 +38,8 @@ else
   $CMAKE -Wdev .. "$@"
 fi
 make -j4
+if [ -n $IS_MACOS ]; then
+  OLDNAME=$(otool -L rwrapper | grep "libR.dylib" | awk '{ print $1 }')
+  install_name_tool -change $OLDNAME @library_path/libR.dylib rwrapper
+fi
 mv rwrapper ../rwrapper-$PACKAGE_NAME
