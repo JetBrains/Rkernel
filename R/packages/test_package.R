c9c09e5e007ca2e134d147d521edb4d573c6bd8d
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2020-02-26 19:26:40 +0300

    [R] Build tools: add button for running tests
    GitOrigin-RevId: 48f194becd3e47aacaa00eecd5bfe19794219a5b

---
+++ R/packages/test_package.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(TRUE)
+
+if (length(args) != 0) {
+  warning("Usage: check_package.R")
+  quit(save = "no", status = 1, runLast = FALSE)
+}
+
+devtools::test()
