ec5fe70d0c852a1f1da57fd390290e06c771feaf
Vladimir Koshelev <vladimir.koshelev@jetbrains.com>
2020-02-28 13:48:53 +0100

    [R] SessionBuild.R: comment hooks
    GitOrigin-RevId: 500c39a5d19e122db7b6d9953a68dfb68b0dc33b

--- R/RSession/modules/SessionBuild.R
+++ R/RSession/modules/SessionBuild.R
@@ -10,16 +10,14 @@
 # ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
 # MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
-#
-#
 
-setHook("sourceCpp.onBuild", function(file, fromCode, showOutput) {
-   .Call("rs_sourceCppOnBuild", file, fromCode, showOutput)
-})
-
-setHook("sourceCpp.onBuildComplete", function(succeeded, output) {
-   .Call("rs_sourceCppOnBuildComplete", succeeded, output)
-})
+# setHook("sourceCpp.onBuild", function(file, fromCode, showOutput) {
+#    .Call("rs_sourceCppOnBuild", file, fromCode, showOutput)
+# })
+#
+# setHook("sourceCpp.onBuildComplete", function(succeeded, output) {
+#    .Call("rs_sourceCppOnBuildComplete", succeeded, output)
+# })
 
 .rs.addFunction("installBuildTools", function(action) {
    response <- .rs.userPrompt(
