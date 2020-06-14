71c1c24cb8016683e8985aec55edd88f31695aa3
Valery Teplyakov <Valeriy.Teplyakov@jetbrains.com>
2020-06-14 18:33:49 +0300

    [R] Pre-compute extra-named-arguments in skeletons
    GitOrigin-RevId: 894a0baf8b377b794083323f89d602f9b5d8a2fd

--- R/package_summary.R
+++ R/package_summary.R
@@ -19,15 +19,20 @@
 
 arguments = commandArgs(TRUE)
 
-if (length(arguments) != 1) {
-  warning("Usage: package_summary.R <package_name>")
+if (length(arguments) != 2) {
+  warning("Usage: package_summary.R <package_name> <extraNamedArguments_path>")
   quit(save = "no", status = 1, runLast = FALSE)
 }
 
-pName = arguments[1]
+attachAndLoadNamespace <- function(pName) {
+  suppressMessages(suppressWarnings(library(pName, character.only = T))) # Attach necessary for extraNamedArgs
+  loadNamespace(pName)
+}
+
+pName = arguments[[1]]
 
 cat(">>>RPLUGIN>>>")
-namespace = tryCatch(loadNamespace(pName), error = function(e) {
+namespace = tryCatch(attachAndLoadNamespace(pName), error = function(e) {
   cat("intellij-cannot-load-package")
   message(e)
   cat("<<<RPLUGIN<<<")
@@ -57,16 +62,39 @@ symbolSignature <- function(symbol, types, isExported, spec = c()) {
   paste(str, collapse='\001')
 }
 
+assign(".jetbrains", new.env(), envir = baseenv())
+source(arguments[[2]]) # extraNamedArguments.R
+cache <- new.env()
+
 for (symbol in allSymbols) {
   if (symbol %in% ignoreList) next
   obj = base::get(symbol, envir = namespace)
 
   types = class(obj)
+  spec <- NULL
   if ("function" %in% types) {
-    spec = c(args(obj)) # I don't know how to transform it into string
-  }
-  else {
-    spec = c()
+    description <- args(obj)
+    if (length(description) > 0) {
+      description <- deparse(description)
+      spec <- paste(description[seq_len(length(description) - 1)], collapse = "")
+      # See extraNamedArguments.R#.jetbrains$findExtraNamedArgs for details
+      extraNamedArgs <- tryCatch(.jetbrains$findExtraNamedArgs(symbol, 2, cache), error = function(e) {
+        write(geterrmessage(), file = stderr())
+      })
+      if (length(extraNamedArgs) > 0) {
+        argNames <- NULL # Names of arguments which can be passed directly to `...`
+        funNames <- NULL # Names of arguments which is function whose arguments can also be passed to `...`
+        lapply(extraNamedArgs, function(x) {
+          if (!x[[2]]) {
+            argNames <<- c(argNames, x[[1]])
+          }
+          else {
+            funNames <<- c(funNames, x[[1]])
+          }
+        })
+        spec <- c(spec, paste(argNames, collapse = ";"), paste(funNames, collapse = ";"))
+      }
+    }
   }
   isExported = symbol %in% exportedSymbols
   cat("\n")
