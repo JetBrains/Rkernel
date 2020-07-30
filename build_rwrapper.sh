653fe83a6119f137cc0dcb1faefcbd738b5ae727
Parviz Khavari <parviz.khavari@jetbrains.com>
2020-07-30 09:43:24 +0300

    [R] Extract getting fsnotifier from build_rwrapper script
    GitOrigin-RevId: 0a24a0ca50d8adba22db0f43b1d6822fe2def3e1

--- build_rwrapper.sh
+++ build_rwrapper.sh
@@ -42,14 +42,4 @@ fi
 mv rwrapper ../rwrapper-$PACKAGE_NAME
 
 cd ..
-if [ ! -f fsnotifier-linux ]; then
-  curl https://raw.githubusercontent.com/JetBrains/intellij-community/master/bin/linux/fsnotifier64 -o fsnotifier-linux
-  chmod +x fsnotifier-linux
-fi
-if [ ! -f fsnotifier-osx ]; then
-  curl https://raw.githubusercontent.com/JetBrains/intellij-community/master/bin/mac/fsnotifier -o fsnotifier-osx
-  chmod +x fsnotifier-osx
-fi
-if [ ! -f fsnotifier-win.exe ]; then
-  curl https://raw.githubusercontent.com/JetBrains/intellij-community/master/bin/win/fsnotifier.exe -o fsnotifier-win.exe
-fi
+./get_fsnotifier.sh
