072cb2ff9a7e6615c5093f2332a1f5ec16a933bc
Mikhail Putilin <mikhail.putilin@jetbrains.com>
2020-10-19 14:03:21 +0300

    [R] Fix enabling debugger in autoprint when it's not needed
    GitOrigin-RevId: 360330eb5b36bdb496e6194edc85ee0dba21a977

--- src/ExecuteCode.cpp
+++ src/ExecuteCode.cpp
@@ -304,9 +304,9 @@ static void executeCodeImpl(SEXP _exprs, SEXP _env, bool withEcho, bool isDebug,
         PROTECT(newEnv = RI->newEnv.invokeUnsafeInEnv(env, named("parent", env)));
         Rf_defineVar(RI->xSymbol, value, newEnv);
         SEXP printExpr = Rf_isFunction(value) && IS_S4_OBJECT(value) ? RI->basePrintDefaultXExpr : RI->basePrintXExpr;
-        rDebugger.enable();
+        if (isDebug) rDebugger.enable();
         Rf_eval(printExpr, newEnv);
-        rDebugger.disable();
+        if (isDebug) rDebugger.disable();
         UNPROTECT(1);
       }
       if (setLastValue) {
