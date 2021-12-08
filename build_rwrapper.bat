f28895025a3f09b91a59ef7bb078adaa9e5cd518
Andrey Vokin <andrey.vokin@jetbrains.com>
2021-12-08 11:22:37 +0100

    [R] fix linking in build_rwrapper.bat
    GitOrigin-RevId: 7fa848ceaa3562c60c3237919f3e272d32ba6ab2

--- build_rwrapper.bat
+++ build_rwrapper.bat
@@ -1,8 +1,17 @@
+set R_HOME=C:\intellij\rplugin\rwrapper\R-4.1.0
+set R_INCLUDE=C:\intellij\rplugin\rwrapper\R-4.1.0\include
+set PATH=C:\intellij\rplugin\rwrapper\R-4.1.0\bin;C:\intellij\rplugin\rwrapper\R-4.1.0\bin\x64;%PATH%
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\vsdevcmd.bat" -arch=amd64
+
 cd rwrapper
 call clone_dependency.bat
+
 mkdir gens\protos
 XCOPY ..\protos\* Rkernel-proto /s /i
-call "C:\Program Files\R\R-4.1.0\bin\x64\Rscript.exe" dll2lib.R
+
+call Rscript.exe dll2lib.R
+
 rmdir /S /Q build
 mkdir build
 cd build
