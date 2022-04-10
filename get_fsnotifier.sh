f4899ec32b0e02068bcff70ded16a362d35aae62
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2022-04-10 14:19:27 +0200

    [R] IJI-844 Support fsnotifier for Linux-aarch64 in RPlugin
    GitOrigin-RevId: 986f54e653d4e5a62d3649edc0bb8678d11331ff

--- get_fsnotifier.sh
+++ get_fsnotifier.sh
@@ -5,6 +5,10 @@ if [ ! -f fsnotifier-linux ]; then
   curl https://raw.githubusercontent.com/JetBrains/intellij-community/master/bin/linux/fsnotifier -o fsnotifier-linux
   chmod +x fsnotifier-linux
 fi
+if [ ! -f fsnotifier-linux-aarch64 ]; then
+  curl https://raw.githubusercontent.com/JetBrains/intellij-community/master/bin/linux/fsnotifier-aarch64 -o fsnotifier-linux-aarch64
+  chmod +x fsnotifier-linux-aarch64
+fi
 if [ ! -f fsnotifier-osx ]; then
   curl https://raw.githubusercontent.com/JetBrains/intellij-community/master/bin/mac/fsnotifier -o fsnotifier-osx
   chmod +x fsnotifier-osx
