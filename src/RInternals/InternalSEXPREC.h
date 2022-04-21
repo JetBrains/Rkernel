5be8287f986da76a762aab8a36383cf7385342a3
Andrey Vokin <andrey.vokin@jetbrains.com>
2022-04-21 13:55:02 +0200

    R-1374 Unsupported interpreter version 4.2.0
    Several C Headers were moved from public include to internal Defn.h file.
    
    See R sources commit: 86b4a0641e889808ce204e28dd8bd19bbd1c8cbb
    Move a number of non-API declarations and macro definitions from the
    installed header Rinternals.h to the private header Defn.h.
    git-svn-id: https://svn.r-project.org/R/trunk@80644 00db46b3-68df-0310-9c12-caf00c1e9a41
    
    (cherry picked from commit f27d85dec0bf5c48bcf5fd3847e1e9d85759f9ee)
    
    IJ-CR-24005
    
    GitOrigin-RevId: 71b0d8f8c5fe686247dfb84c9246e62d04223a48

--- src/RInternals/InternalSEXPREC.h
+++ src/RInternals/InternalSEXPREC.h
@@ -23,77 +23,8 @@
 #define NAMED_BITS 16
 
 extern "C" {
-struct sxpinfo_struct {
-  SEXPTYPE type      :  TYPE_BITS;
-  unsigned int scalar:  1;
-  unsigned int obj   :  1;
-  unsigned int alt   :  1;
-  unsigned int gp    : 16;
-  unsigned int mark  :  1;
-  unsigned int debug :  1;
-  unsigned int trace :  1;
-  unsigned int spare :  1;
-  unsigned int gcgen :  1;
-  unsigned int gccls :  3;
-  unsigned int named : NAMED_BITS;
-  unsigned int extra : 32 - NAMED_BITS;
-};
 
-struct vecsxp_struct {
-  R_xlen_t length;
-  R_xlen_t truelength;
-};
 
-struct primsxp_struct {
-  int offset;
-};
-
-struct symsxp_struct {
-  struct SEXPREC *pname;
-  struct SEXPREC *value;
-  struct SEXPREC *internal;
-};
-
-struct listsxp_struct {
-  struct SEXPREC *carval;
-  struct SEXPREC *cdrval;
-  struct SEXPREC *tagval;
-};
-
-struct envsxp_struct {
-  struct SEXPREC *frame;
-  struct SEXPREC *enclos;
-  struct SEXPREC *hashtab;
-};
-
-struct closxp_struct {
-  struct SEXPREC *formals;
-  struct SEXPREC *body;
-  struct SEXPREC *env;
-};
-
-struct promsxp_struct {
-  struct SEXPREC *value;
-  struct SEXPREC *expr;
-  struct SEXPREC *env;
-};
-
-#define SEXPREC_HEADER \
-    struct sxpinfo_struct sxpinfo; \
-    struct SEXPREC *attrib; \
-    struct SEXPREC *gengc_next_node, *gengc_prev_node
-
-typedef struct SEXPREC {
-  SEXPREC_HEADER;
-  union {
-    struct primsxp_struct primsxp;
-    struct symsxp_struct symsxp;
-    struct listsxp_struct listsxp;
-    struct envsxp_struct envsxp;
-    struct closxp_struct closxp;
-    struct promsxp_struct promsxp;
-  } u;
-} SEXPREC_impl;
 
 };
 
--- src/RInternals/RInternals.h
+++ src/RInternals/RInternals.h
@@ -63,3 +63,112 @@ SEXP matchArgs(SEXP formals, SEXP supplied, SEXP call);
 int getRIntVersion();
 
 #endif //RWRAPPER_RINTERNALS_H
+
+#ifndef DEFN_H_
+#define DEFN_H_
+
+
+#define NAMED_BITS 16
+
+struct sxpinfo_struct {
+    SEXPTYPE type      :  TYPE_BITS;
+                            /* ==> (FUNSXP == 99) %% 2^5 == 3 == CLOSXP
+			     * -> warning: `type' is narrower than values
+			     *              of its type
+			     * when SEXPTYPE was an enum */
+    unsigned int scalar:  1;
+    unsigned int obj   :  1;
+    unsigned int alt   :  1;
+    unsigned int gp    : 16;
+    unsigned int mark  :  1;
+    unsigned int debug :  1;
+    unsigned int trace :  1;  /* functions and memory tracing */
+    unsigned int spare :  1;  /* used on closures and when REFCNT is defined */
+    unsigned int gcgen :  1;  /* old generation number */
+    unsigned int gccls :  3;  /* node class */
+    unsigned int named : NAMED_BITS;
+    unsigned int extra : 32 - NAMED_BITS; /* used for immediate bindings */
+}; /*		    Tot: 64 */
+
+struct primsxp_struct {
+    int offset;
+};
+
+struct symsxp_struct {
+    struct SEXPREC *pname;
+    struct SEXPREC *value;
+    struct SEXPREC *internal;
+};
+
+struct listsxp_struct {
+    struct SEXPREC *carval;
+    struct SEXPREC *cdrval;
+    struct SEXPREC *tagval;
+};
+
+struct envsxp_struct {
+    struct SEXPREC *frame;
+    struct SEXPREC *enclos;
+    struct SEXPREC *hashtab;
+};
+
+struct closxp_struct {
+    struct SEXPREC *formals;
+    struct SEXPREC *body;
+    struct SEXPREC *env;
+};
+
+struct promsxp_struct {
+    struct SEXPREC *value;
+    struct SEXPREC *expr;
+    struct SEXPREC *env;
+};
+
+
+#define SEXPREC_HEADER \
+    struct sxpinfo_struct sxpinfo; \
+    struct SEXPREC *attrib; \
+    struct SEXPREC *gengc_next_node, *gengc_prev_node
+
+/* The standard node structure consists of a header followed by the
+   node data. */
+typedef struct SEXPREC {
+    SEXPREC_HEADER;
+    union {
+	struct primsxp_struct primsxp;
+	struct symsxp_struct symsxp;
+	struct listsxp_struct listsxp;
+	struct envsxp_struct envsxp;
+	struct closxp_struct closxp;
+	struct promsxp_struct promsxp;
+    } u;
+} SEXPREC_impl;
+
+#define TAG(e)		((e)->u.listsxp.tagval)
+
+/* Bytecode access macros */
+#define BCODE_CODE(x)	CAR(x)
+//#define BCODE_CONSTS(x) CDR(x)
+#define BCODE_EXPR(x)	TAG(x)
+
+
+
+#define MISSING_MASK	15 /* reserve 4 bits--only 2 uses now */
+#define MISSING(x)	((x)->sxpinfo.gp & MISSING_MASK)/* for closure calls */
+#define SET_MISSING(x,v) do { \
+  SEXP __x__ = (x); \
+  int __v__ = (v); \
+  int __other_flags__ = __x__->sxpinfo.gp & ~MISSING_MASK; \
+  __x__->sxpinfo.gp = __other_flags__ | __v__; \
+} while (0)
+
+extern "C" Rboolean Rf_pmatch(SEXP, SEXP, Rboolean);
+
+Rboolean Rf_psmatch(const char *, const char *, Rboolean);
+void Rf_printwhere(void);
+void Rf_readS3VarsFromFrame(SEXP, SEXP*, SEXP*, SEXP*, SEXP*, SEXP*, SEXP*);
+
+
+#define pmatch			Rf_pmatch
+
+#endif
