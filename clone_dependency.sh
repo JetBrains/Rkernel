55534a3969d0d3f48b025f31b69453ea39cb1a0c
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2022-04-25 18:20:20 +0200

    [R] rwrapper: update vcpkg for linux-arm64
    GitOrigin-RevId: 79b851131d109a79e704384f1e0695dd128f3163

--- clone_dependency.sh
+++ clone_dependency.sh
@@ -5,7 +5,7 @@ set -e
 cd "$(dirname "$0")"
 
 if [ ! -d vcpkg ]; then
-  if [ "$OS" == "Darwin" ] && [ "$ARCH" == "arm64" ]; then
+  if [ "$ARCH" == "arm64" ]; then
     git clone --single-branch --branch 2021.05.12 --depth 1 https://github.com/microsoft/vcpkg.git
   else
     git clone --single-branch --branch 2019.12 --depth 1 https://github.com/microsoft/vcpkg.git
