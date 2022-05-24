e6c4ae02bc827cf414b3220a79c4e81d7d3e8ee1
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2022-05-24 13:22:12 +0200

    [R] rwrapper: improve build scripts for grpc for windows
    GitOrigin-RevId: e1893c96f510e5eda51d07404f8c3defc872a157

--- build_grpc.bat
+++ build_grpc.bat
@@ -1,7 +1,7 @@
 cd vcpkg
-call .\bootstrap-vcpkg.bat
-.\vcpkg.exe install grpc:x64-windows-static
-.\vcpkg.exe install tiny-process-library:x64-windows-static
+call .\bootstrap-vcpkg.bat || exit /b %ERRORLEVEL%
+.\vcpkg.exe install grpc[codegen,core]:x64-windows-static || exit /b %ERRORLEVEL%
+.\vcpkg.exe install tiny-process-library:x64-windows-static || exit /b %ERRORLEVEL%
 RD /S /Q ..\GRPC
-MOVE /Y installed ..\GRPC
+MOVE /Y installed ..\GRPC || exit /b 3
 
