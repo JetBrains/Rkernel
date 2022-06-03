f311d3b8846201b173b4f17af9c462a567319ca0
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2022-06-03 09:46:22 +0200

    [r] preparations for `fsnotifier` relocation
    (cherry picked from commit cea9e96f6ab51e070f36c5eaff3ba1963a062625)
    
    IJ-CR-25218
    
    GitOrigin-RevId: 8c8a65c8ace64384777df804a803996e5132cee8

--- get_fsnotifier.sh
+++ get_fsnotifier.sh
@@ -2,11 +2,13 @@
 set -euo pipefail
 
 if [ ! -f fsnotifier-linux ]; then
-  curl https://raw.githubusercontent.com/JetBrains/intellij-community/master/bin/linux/fsnotifier -o fsnotifier-linux
+  curl https://raw.githubusercontent.com/JetBrains/intellij-community/master/bin/linux/amd4/fsnotifier -o fsnotifier-linux -f || \
+    curl https://raw.githubusercontent.com/JetBrains/intellij-community/master/bin/linux/fsnotifier -o fsnotifier-linux
   chmod +x fsnotifier-linux
 fi
 if [ ! -f fsnotifier-linux-aarch64 ]; then
-  curl https://raw.githubusercontent.com/JetBrains/intellij-community/master/bin/linux/fsnotifier-aarch64 -o fsnotifier-linux-aarch64
+  curl https://raw.githubusercontent.com/JetBrains/intellij-community/master/bin/linux/aarch64/fsnotifier -o fsnotifier-linux-aarch64 -f || \
+    curl https://raw.githubusercontent.com/JetBrains/intellij-community/master/bin/linux/fsnotifier-aarch64 -o fsnotifier-linux-aarch64
   chmod +x fsnotifier-linux-aarch64
 fi
 if [ ! -f fsnotifier-osx ]; then
