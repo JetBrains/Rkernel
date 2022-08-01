ba3de0bf471ec669d71c84efa5134cf3125eb051
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2022-08-01 19:46:09 +0200

    [r] replacing certain short links
    (cherry picked from commit 3516e27a7cd7a45b664b67917bba058911633e5a)
    
    IJ-CR-27925
    
    GitOrigin-RevId: 98ec7891d5feab8356ea2097f133d62964fe55ec

--- get_fsnotifier.sh
+++ get_fsnotifier.sh
@@ -2,17 +2,17 @@
 set -euo pipefail
 
 if [ ! -f fsnotifier-linux ]; then
-  curl -L https://jb.gg/fsnotifier-linux-amd64 -o fsnotifier-linux
+  curl -L https://intellij.com/fsnotifier-linux-amd64 -o fsnotifier-linux
   chmod +x fsnotifier-linux
 fi
 if [ ! -f fsnotifier-linux-aarch64 ]; then
-  curl -L https://jb.gg/fsnotifier-linux-aarch64 -o fsnotifier-linux-aarch64
+  curl -L https://intellij.com/fsnotifier-linux-aarch64 -o fsnotifier-linux-aarch64
   chmod +x fsnotifier-linux-aarch64
 fi
 if [ ! -f fsnotifier-osx ]; then
-  curl -L https://jb.gg/fsnotifier-macos -o fsnotifier-osx
+  curl -L https://intellij.com/fsnotifier-macos -o fsnotifier-osx
   chmod +x fsnotifier-osx
 fi
 if [ ! -f fsnotifier-win.exe ]; then
-  curl -L https://jb.gg/fsnotifier-win-amd64 -o fsnotifier-win.exe
+  curl -L https://intellij.com/fsnotifier-win-amd64 -o fsnotifier-win.exe
 fi
