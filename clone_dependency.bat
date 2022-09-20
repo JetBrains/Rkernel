0e9060efc3667fcaa6a6ed8772aa8d6758cffffd
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2022-09-20 11:54:03 +0200

    [R] rwrapper: improve build scripts for grpc for windows: fix `git apply`
    GitOrigin-RevId: 01722ffe1c011d389d7f2361f9b94a039e0d3880

--- clone_dependency.bat
+++ clone_dependency.bat
@@ -6,7 +6,7 @@ if not exist Rcpp (
 
 if not exist vcpkg (
   git clone --single-branch --branch 2022.05.10  https://github.com/microsoft/vcpkg.git
-  git -C vcpkg apply ../vcpkg-patches/nasm.diff
+  git -C vcpkg apply --reject --whitespace=fix --ignore-space-change --ignore-whitespace ../vcpkg-patches/nasm.diff
 )
 
 
