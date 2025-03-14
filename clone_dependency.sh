5461aa71802e02318a36cf41cff7a86c85f92092
Willem Stuijt <willem.stuijt@jetbrains.com>
2025-03-14 19:45:56 +0000

    Upgrade rwrapper vcpkg version
    Merge-request: IJ-MR-157780
    Merged-by: Willem Stuijt <willem.stuijt@jetbrains.com>
    
    GitOrigin-RevId: e4d7f113a36bca74222f61d32db9d8855ac6c451

--- clone_dependency.sh
+++ clone_dependency.sh
@@ -5,5 +5,5 @@ set -e
 cd "$(dirname "$0")"
 
 if [ ! -d vcpkg ]; then
-  git clone --single-branch --branch 2022.05.10 --depth 1 https://github.com/microsoft/vcpkg.git
+  git clone --single-branch --branch 2025.02.14 --depth 1 https://github.com/microsoft/vcpkg.git
 fi
